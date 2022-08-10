335967cd009d479b7cc0a15430ead1c69dd07c67
Satyabrata Sahoo <satyabratasahoo@MBPro.local>
2022-08-10 08:36:58 +0530

    code updated to heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist/project'));
+app.use(express.static(__dirname + '/dist/news_seed'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/project/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/news_seed/index.html'));
 })
 
 app.listen(port, () => {
